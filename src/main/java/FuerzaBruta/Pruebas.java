c8e4ea83e076df8a14a2a72981047354e1832500
patgeo117 <gpatino017@gmail.com>
2023-12-17 16:30:09 -0500

    SolucionParcial2.0

--- src/main/java/FuerzaBruta/Pruebas.java
+++ src/main/java/FuerzaBruta/Pruebas.java
@@ -1,11 +1,14 @@
 package FuerzaBruta;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Pruebas {
+
     public static void main(String[] args) {
-        int n = 6;
+        int n = 8;
         int[][] calendario = generarCalendario(n);
         imprimirCalendario(calendario);
     }
@@ -28,9 +31,9 @@ public class Pruebas {
         return calendario;
     }
 
-    private static final int MAX_INTENTOS = 200000000;
+    private static int[][] generarMitadCalendario(int n) {
+       int MAX_INTENTOS = 100000000;// 20000000
 
-    public static int[][] generarMitadCalendario(int n) {
         int[][] mitadCalendario = new int[(n - 1)][n];
         int intentos = 0;
         boolean generado = false;
@@ -42,7 +45,7 @@ public class Pruebas {
             }
             intentos++;
 
-            if (checkNoDuplicatesColumns(mitadCalendario) && checkNoValuesInColumns(mitadCalendario)) {
+            if (checkNoDuplicatesValue(mitadCalendario) && checkNoValuesInColumns(mitadCalendario)) {
                 generado = true;
             }
         }
@@ -51,61 +54,94 @@ public class Pruebas {
             System.out.println("Se alcanzó el límite de intentos.");
             // Reiniciar la matriz y el contador de intentos para un nuevo intento.
             mitadCalendario = new int[(n - 1)][n];
-            intentos = 0;
             generarCalendario(n);
         }
 
         return mitadCalendario;
     }
+    /**
+     * Verifica que no haya valores duplicados en las columnas de la matriz.
+     * @param matrix La matriz a verificar.
+     * @return true si no hay duplicados, false en caso contrario.
+     */
+    private static boolean checkNoDuplicatesValue(int[][] matrix) {
+        int n = matrix[0].length;
 
-    private static boolean checkNoDuplicatesColumns(int[][] matrix) {
-        int filas = matrix.length;
-        int columnas = matrix[0].length;
-
-        for (int j = 0; j < columnas; j++) {
-            for (int i = 0; i < filas - 1; i++) {
-                for (int k = i + 1; k < filas; k++) {
-                    if (matrix[i][j] == matrix[k][j]) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private static boolean checkNoValuesInColumns(int[][] matrix) {
-        int filas = matrix.length;
-        int columnas = matrix[0].length;
-
-        for (int j = 0; j < columnas; j++) {
-            for (int i = 0; i < filas; i++) {
-                if (matrix[i][j] == j + 1) {
+        for (int j = 0; j < n; j++) {
+            Set<Integer> valuesColumn = new HashSet<>();
+            for (int[] ints : matrix) {
+                if (!valuesColumn.add(ints[j])) {
                     return false;
                 }
             }
         }
+        return true;
+    }
+    /**
+     * Verificar que los valores no coincidan con la columna .
+     * @param matrix La matriz a verificar.
+     * @return true si no hay duplicados, false en caso contrario.
+     */
+    private static boolean checkNoValuesInColumns(int[][] matrix) {
+        //int filas = matrix.length;
+        int columnas = matrix[0].length;
 
+        for (int j = 0; j < columnas; j++) {
+            for (int[] ints : matrix) {
+                if (ints[j] == j + 1) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
     public static int[] generarPermutacion(int n) {
-        int[] permutation = new int[n];
+        int MAX_INTENTOS_PERMUTACION = factorial(n);
 
-        for (int i = 0; i < n; i++) {
-            permutation[i] = i + 1;
-        }
-        Random random = new Random();
-        for (int i = n - 1; i > 0; i--) {
-            int j = random.nextInt(i + 1);
-            int temp = permutation[i];
-            if (permutation[i] != permutation[j]) {
-                permutation[i] = permutation[j];
-                permutation[j] = temp;
+        int[] permutation = new int[n];
+        boolean permutacionValida = false;
+        int intentos = 0;
+
+        while (!permutacionValida && (intentos < MAX_INTENTOS_PERMUTACION)) {
+            for (int i = 0; i < n; i++) {
+                permutation[i] = i + 1;
+            }
+
+            Random random = new Random();
+            for (int i = n - 1; i > 0; i--) {
+                int j = random.nextInt(i + 1);
+                int temp = permutation[i];
+                if (permutation[i] != permutation[j]) {
+                    permutation[i] = permutation[j];
+                    permutation[j] = temp;
+                }
+            }
+
+            // Verificar la validez de la permutación
+            if (!checkNoValuesInColumns(new int[][]{permutation})) {
+                intentos++;
+            } else {
+                permutacionValida = true;
             }
         }
+
+        if (intentos >= MAX_INTENTOS_PERMUTACION) {
+            System.out.println("Se alcanzó el límite de intentos para generar permutaciones válidas.");
+            // Puedes manejar esto según tus necesidades, por ejemplo, lanzar una excepción o retornar un valor predeterminado.
+            return new int[n];
+        }
+
         return permutation;
+    }
+
+    private static int factorial(int n) {
+        int factorial = 1;
+        for (int i = 2; i <= n; i++) {
+            factorial *= i;
+        }
+
+        return factorial;
     }
 
     public static void imprimirCalendario(int[][] calendario) {
