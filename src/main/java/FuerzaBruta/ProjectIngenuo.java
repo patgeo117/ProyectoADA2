6d47a9a9d1537b86e43827bd888686026678cdfc
patgeo117 <gpatino017@gmail.com>
2023-12-18 18:45:58 -0500

    SolucionParcial 3.0

--- src/main/java/FuerzaBruta/ProjectIngenuo.java
+++ src/main/java/FuerzaBruta/ProjectIngenuo.java
@@ -1,16 +1,19 @@
 package FuerzaBruta;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 /**
- * Este programa genera un calendario de partidos de fútbol de ida y vuelta
- * para un número par de equipos.
- * El calendario generado no tiene partidos repetidos ni equipos que jueguen contra si mismos.
+ * Esta clase representa un programa que genera un calendario de partidos de fútbol de ida y vuelta
+ * para un número par de equipos. El calendario generado no tiene partidos repetidos ni equipos que jueguen
+ * contra sí mismos.
  */
 public class ProjectIngenuo {
+
+    /**
+     * Método principal que inicia la ejecución del programa.
+     *
+     * @param args Argumentos de la línea de comandos (no se utilizan en este programa).
+     */
     public static void main(String[] args) {
         int n = 6;
 
@@ -23,11 +26,11 @@ public class ProjectIngenuo {
         } catch (NumeroEquiposImparException e) {
             System.out.println("Error: El número de equipos debe ser par.");
         }
-
     }
 
     /**
      * Genera un calendario de partidos de fútbol de ida y vuelta para un número par de equipos.
+     *
      * @param n El número de equipos.
      * @return Una matriz de n x 2(n-1) con los partidos de fútbol de ida y vuelta.
      */
@@ -36,6 +39,13 @@ public class ProjectIngenuo {
         return combinarCalendario(n, mitadCalendario);
     }
 
+    /**
+     * Combina la mitad del calendario generada para obtener los partidos de ida y vuelta.
+     *
+     * @param n              El número de equipos.
+     * @param mitadCalendario La mitad del calendario generada.
+     * @return Una matriz con los partidos de fútbol de ida y vuelta.
+     */
     public static int[][] combinarCalendario(int n, int[][] mitadCalendario) {
         int[][] calendario = new int[2 * (n - 1)][n];
 
@@ -45,40 +55,42 @@ public class ProjectIngenuo {
                 calendario[i + n - 1][j] = -mitadCalendario[i][j];
             }
         }
-
         return calendario;
     }
 
+    /**
+     * Genera la mitad del calendario aleatorio para un número par de equipos.
+     *
+     * @param n El número de equipos.
+     * @return La mitad del calendario generada.
+     */
     private static int[][] generarMitadCalendario(int n) {
-       int MAX_INTENTOS = 100000000;// 20000000
-
+        int MAX_INTENTOS = 1000000000;
         int[][] mitadCalendario = new int[(n - 1)][n];
-        int intentos = 0;
-        boolean generado = false;
 
-        while (!generado && (intentos < MAX_INTENTOS)) {
+        int intentos = 0;
+
+        while (intentos < MAX_INTENTOS) {
             for (int i = 0; i < n - 1; i++) {
-                int[] permutacion = generarPermutacion(n);
-                mitadCalendario[i] = permutacion;
+                mitadCalendario[i] = generarPermutacion(n);
             }
             intentos++;
 
             if (checkNoDuplicatesValue(mitadCalendario) && checkNoValuesInColumns(mitadCalendario)) {
-                generado = true;
+                for (int[] i : mitadCalendario) {
+                    System.out.println(Arrays.toString(i));
+                }
+                System.out.println("--------------------");
+                return mitadCalendario;
             }
         }
 
-        if (intentos >= MAX_INTENTOS) {
-            System.out.println("Se alcanzó el límite de intentos.");
-            // Reiniciar la matriz y el contador de intentos para un nuevo intento.
-            mitadCalendario = new int[(n - 1)][n];
-            generarCalendario(n);
-        }
-
-        return mitadCalendario;
+        throw new RuntimeException("Se alcanzó el límite de intentos sin generar un calendario válido.");
     }
+
     /**
      * Verifica que no haya valores duplicados en las columnas de la matriz.
+     *
      * @param matrix La matriz a verificar.
      * @return true si no hay duplicados, false en caso contrario.
      */
@@ -95,13 +107,14 @@ public class ProjectIngenuo {
         }
         return true;
     }
+
     /**
-     * Verificar que los valores no coincidan con la columna .
+     * Verifica que los valores no coincidan con la columna.
+     *
      * @param matrix La matriz a verificar.
      * @return true si no hay duplicados, false en caso contrario.
      */
     private static boolean checkNoValuesInColumns(int[][] matrix) {
-        //int filas = matrix.length;
         int columnas = matrix[0].length;
 
         for (int j = 0; j < columnas; j++) {
@@ -114,45 +127,69 @@ public class ProjectIngenuo {
         return true;
     }
 
+    /**
+     * Genera una permutación aleatoria de números del 1 al n.
+     *
+     * @param n El número de elementos en la permutación.
+     * @return La permutación generada.
+     */
     public static int[] generarPermutacion(int n) {
-        int MAX_INTENTOS_PERMUTACION = factorial(n);
+        List<Integer> permutationList = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            permutationList.add(i);
+        }
 
-        int[] permutation = new int[n];
-        boolean permutacionValida = false;
         int intentos = 0;
+        int maxIntentos = factorial(n);
 
-        while (!permutacionValida && (intentos < MAX_INTENTOS_PERMUTACION)) {
+        int[] check = new int[n];
+
+        do {
+            Collections.shuffle(permutationList);
             for (int i = 0; i < n; i++) {
-                permutation[i] = i + 1;
+                check[i] = permutationList.get(i);
             }
+            intentos++;
+        } while (!esPermutacionValida(permutationList) && intentos < maxIntentos);
 
-            Random random = new Random();
-            for (int i = n - 1; i > 0; i--) {
-                int j = random.nextInt(i + 1);
-                int temp = permutation[i];
-                if (permutation[i] != permutation[j]) {
-                    permutation[i] = permutation[j];
-                    permutation[j] = temp;
-                }
-            }
-
-            // Verificar la validez de la permutación
-            if (!checkNoValuesInColumns(new int[][]{permutation})) {
-                intentos++;
-            } else {
-                permutacionValida = true;
-            }
-        }
-
-        if (intentos >= MAX_INTENTOS_PERMUTACION) {
+        if (intentos >= maxIntentos) {
             System.out.println("Se alcanzó el límite de intentos para generar permutaciones válidas.");
-            // Puedes manejar esto según tus necesidades, por ejemplo, lanzar una excepción o retornar un valor predeterminado.
-            return new int[n];
+            return new int[n]; // O lanzar una excepción según tus necesidades.
         }
 
-        return permutation;
+        return checkGameEquipos(check);
     }
 
+    private static boolean esPermutacionValida(List<Integer> permutation) {
+        for (int i = 0; i < permutation.size(); i++) {
+            if (permutation.get(i) == i + 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] checkGameEquipos(int[] permutaciones){
+        boolean aux = true;
+        for(int i: permutaciones){
+            for(int j = 0; j<permutaciones.length;j++){
+                if(i == permutaciones[j]){
+                    permutaciones[i-1] = j+1;
+                }
+                if (aux = true){
+                    permutaciones[i-1] = permutaciones[i-1] * -1;
+                }
+                
+            }
+        }
+        return permutaciones;
+    }
+    /**
+     * Calcula el factorial de un número.
+     *
+     * @param n El número para calcular el factorial.
+     * @return El factorial de n.
+     */
     private static int factorial(int n) {
         int factorial = 1;
         for (int i = 2; i <= n; i++) {
@@ -162,22 +199,41 @@ public class ProjectIngenuo {
         return factorial;
     }
 
+    /**
+     * Imprime el calendario de partidos en la consola.
+     *
+     * @param calendario La matriz que representa el calendario de partidos.
+     */
     public static void imprimirCalendario(int[][] calendario) {
         for (int[] i : calendario) {
             System.out.println(Arrays.toString(i));
         }
     }
 
+    /**
+     * Valida que el número de equipos sea par. En caso contrario, lanza una excepción.
+     *
+     * @param n El número de equipos.
+     */
     private static void validarNumeroEquipos(int n) {
         if (n % 2 != 0) {
             throw new NumeroEquiposImparException("El número de equipos debe ser par.");
         }
     }
 
+    /**
+     * Excepción personalizada para indicar que el número de equipos es impar.
+     */
     public static class NumeroEquiposImparException extends RuntimeException {
+        /**
+         * Construye una nueva instancia de la excepción con el mensaje especificado.
+         *
+         * @param message El mensaje de la excepción.
+         */
         public NumeroEquiposImparException(String message) {
             super(message);
         }
     }
 }
+
 
